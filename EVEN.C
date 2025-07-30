a1ba369f51f4a783ca457f9c702e620c8a5a6e56
Rohan Renuse <rohanrenuse1961@gmail.com>
2025-07-30 11:23:24 +0530

    Add files via upload

---
+++ EVEN.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter the number of even numbers to print: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        printf("%d\n", 2 * i);
+    }
+
+    return 0;
+}
