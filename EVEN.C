6b9829d8d1d8514397142dabf3fd679bee9d1943
Rohan Renuse <rohanrenuse1961@gmail.com>
2025-07-30 11:27:10 +0530

    Update EVEN.C

--- EVEN.C
+++ EVEN.C
@@ -2,12 +2,10 @@
 
 int main() {
     int n, i;
-
     printf("Enter the number of even numbers to print: ");
     scanf("%d", &n);
-
     for(i = 1; i <= n; i++) {
-        printf("%d\n", 2 * i);
+        printf("%d\n", 2*i);
     }
 
     return 0;
